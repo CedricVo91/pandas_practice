5808db1537172f8b68a3180d09a273a1ee7aee62
CedricVo91 <cedricvogt91@gmail.com>
2022-12-30 09:19:03 +0100

    final try to upload final kaggle pandas tutorial exercise

---
+++ .ipynb_checkpoints/pandas_practice-checkpoint.py
@@ -0,0 +1,12 @@
+# pandas
+# numpy
+#if I create a conda environment, do I need to install all the data science packages like pandas etc again even though they are already installed in my "global" conda environment
+#if yes, is it correct to use conda install pandas?
+
+
+import pandas as pd
+import numpy as np
+print("hello conda environment and pandas")
+
+array1 = np.array([1,2,3,4])
+print(f"I will print my first array {array1}")
